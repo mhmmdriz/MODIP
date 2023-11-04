d74008c201bbec12d0f7d802f83d17e4dedf3c7d
mhmmdriz <rizki25030@gmail.com>
2023-11-04 14:21:50 +0700

    unfinished irs doswal feature

--- resources/views/dosenwali/dashboard.blade.php
+++ resources/views/dosenwali/dashboard.blade.php
@@ -6,18 +6,18 @@
     <div class="card mb-3 bg-body-tertiary">
       <div class="row">
         <div class="col-md-auto m-4">
-          <img src="{{ asset("storage/profile_photo/default.jpg") }}" alt="" style="border-radius:50%; width:120px">
+          <img src="/photo/private/profile_photo/default.jpg" alt="" style="border-radius:50%; width:120px">
         </div>
         <div class="col m-4">
           <div class="row">
             <div class="col">
-              <h5><b>Operator Departemen Informatika</b></h5>
+              <h5><b>{{ auth()->user()->dosen_wali->nama }}</b></h5>
             </div>
           </div>
           <hr>
           <div class="row">
             <div class="col">
-              Fakultas Sains dan Matematika
+              NIP : {{ auth()->user()->dosen_wali->nip }}
             </div>
           </div>
         </div>
@@ -28,16 +28,16 @@
 
 <div class="row d-flex gx-5 gy-2">
   <div class="col-md-4">
-    <a href="" style="text-decoration: none">
+    <a href="/irsPerwalian" style="text-decoration: none">
       <div class="card mb-3 bg-body-tertiary">
         <div class="row d-flex justify-content-center mt-2">
           <div class="col-md-auto">
-            <h5><b>Akun Mahasiswa</b></h5>
+            <h5><b>IRS Mahasiswa Perwalian</b></h5>
           </div>
         </div>
         <div class="row d-flex justify-content-center mb-2" style="margin-top:-10px">
           <div class="col-md-auto">
-            <i class="bi bi-mortarboard-fill" style="font-size:70px;"></i>
+            <i class="bi bi-book-fill" style="font-size:70px;"></i>
           </div>
         </div>
       </div>
@@ -48,12 +48,12 @@
       <div class="card mb-3 bg-body-tertiary">
         <div class="row d-flex justify-content-center mt-2">
           <div class="col-md-auto">
-            <h5><b>Akun Dosen Wali</b></h5>
+            <h5><b>KHS Mahasiswa Perwalian</b></h5>
           </div>
         </div>
         <div class="row d-flex justify-content-center mb-2" style="margin-top:-10px">
           <div class="col-md-auto">
-            <i class="bi bi-suitcase-lg-fill" style="font-size:70px;"></i>
+            <i class="bi bi-file-earmark-text-fill" style="font-size:70px;"></i>
           </div>
         </div>
       </div>
@@ -64,12 +64,12 @@
       <div class="card mb-3 bg-body-tertiary">
         <div class="row d-flex justify-content-center mt-2">
           <div class="col-md-auto">
-            <h5><b>Akun Departemen</b></h5>
+            <h5><b>PKL Mahasiswa Perwalian</b></h5>
           </div>
         </div>
         <div class="row d-flex justify-content-center mb-2" style="margin-top:-10px">
           <div class="col-md-auto">
-            <i class="bi bi-building-fill" style="font-size:70px;"></i>
+            <i class="bi bi-person-workspace" style="font-size:70px;"></i>
           </div>
         </div>
       </div>
@@ -80,28 +80,12 @@
       <div class="card mb-3 bg-body-tertiary">
         <div class="row d-flex justify-content-center mt-2">
           <div class="col-md-auto">
-            <h5><b>Kunci Pengisian Data</b></h5>
+            <h5><b>Skripsi Mahasiswa Perwalian</b></h5>
           </div>
         </div>
         <div class="row d-flex justify-content-center mb-2" style="margin-top:-10px">
           <div class="col-md-auto">
-            <i class="bi bi-lock-fill" style="font-size:70px;"></i>
-          </div>
-        </div>
-      </div>
-    </a>
-  </div>
-  <div class="col-md-4">
-    <a href="" style="text-decoration: none">
-      <div class="card mb-3 bg-body-tertiary">
-        <div class="row d-flex justify-content-center mt-2">
-          <div class="col-md-auto">
-            <h5><b>Kirim Notifikasi</b></h5>
-          </div>
-        </div>
-        <div class="row d-flex justify-content-center mb-2" style="margin-top:-10px">
-          <div class="col-md-auto">
-            <i class="bi bi-bell-fill" style="font-size:70px;"></i>
+            <i class="bi bi-journal-text" style="font-size:70px;"></i>
           </div>
         </div>
       </div>
---
+++ resources/views/dosenwali/irs/index.blade.php
@@ -0,0 +1,42 @@
+@extends('templates.main')
+
+@section('container')
+<div class="row d-flex justify-content-center my-3">
+  <div class="col-auto">
+    <h5>Data IRS Mahasiswa Perwalian</h5>
+  </div>
+</div>
+
+<div class="card bg-body-tertiary mb-3">
+  <div class="row my-3 mx-2">
+    <div class="col-auto">
+      <h5>Daftar Angkatan</h5>
+    </div>
+  </div>
+
+  @foreach ($data_mhs as $mhs)
+  <a href="/irsPerwalian/{{ $mhs->angkatan }}" class="text-decoration-none" style="color:inherit;">
+    <div class="row mx-3 mb-3">
+      <div class="col bg-light-subtle rounded border">
+        <div class="row">
+          <div class="col-auto border-end py-4 text-center" >
+            Angkatan {{ $mhs->angkatan }}
+          </div>
+          <div class="col-auto py-4 text-center ">
+            Jumlah Mahasiswa: {{ $mhs->jumlah_mhs }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </a>
+  @endforeach
+
+  
+
+</div>
+
+@include('mahasiswa.irs.modal_edit_irs')
+@include('mahasiswa.irs.modal_alert')
+<script src="js/modal.js"></script>
+
+@endsection
