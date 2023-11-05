a00288c185b8f1d4aff93c4efa0b60342566f517
mhmmdriz <rizki25030@gmail.com>
2023-11-05 23:51:05 +0700

    update pencarian progress studi mhs feature

--- resources/views/departemen/pencarian_progress/modal_irs.blade.php
+++ resources/views/departemen/pencarian_progress/modal_irs.blade.php
@@ -10,7 +10,10 @@
         <p class="m-0 sks-irs"></p>
         <br>
         <p class="m-0"><b>Scan IRS</b></p>
-        <a href="" class="m-0 link-scan-irs" target="__blank"></a>
+        <a href="" class="m-0 link-scan-irs d-block" target="__blank"></a>
+        <br>
+        <p class="m-0"><b>Validasi</b></p>
+        <p class="m-0 validasi-irs"></p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalMain">Back</button>
